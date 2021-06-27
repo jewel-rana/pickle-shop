d611d11fa35e10a3e3ed78ca1bd970199137a791
Jewel Rana <jewelrana.dev@gmail.com>
2021-06-27 18:12:04 +0600

    Created product factory

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Database\Factories\ProductFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
@@ -11,6 +12,11 @@ class Product extends Model
     use HasFactory;
 
     protected $fillable = ['name', 'description'];
+
+    protected static function newFactory(): ProductFactory
+    {
+        return ProductFactory::new();
+    }
 
     public function productVariants(): HasMany
     {
