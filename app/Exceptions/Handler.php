bd4173a2fa1dc1728548b32169de5b3d27f0a5e9
Jewel Rana <jewelrana.dev@gmail.com>
2021-06-27 23:54:19 +0600

    Exception handller render method added

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,7 +2,9 @@
 
 namespace App\Exceptions;
 
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Validation\ValidationException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -37,5 +39,19 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    public function render($request, Throwable $exception)
+    {
+        if ($exception instanceof ModelNotFoundException) {
+            return response()->error(__('Model not found'), $exception->getMessage());
+        }
+        if($exception instanceof ValidationException) {
+            return response()->error(__('Validation failed'), $exception->getMessage());
+        }
+        if ($request->wantsJson()) {
+            return parent::prepareJsonResponse($request, $exception);
+        }
+        return parent::render($request, $exception);
     }
 }
