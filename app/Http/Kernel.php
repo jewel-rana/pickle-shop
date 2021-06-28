2bc19b53526e9ab7bd8fa113d755907772d60288
Jewel Rana <jewelrana.dev@gmail.com>
2021-06-28 16:08:30 +0600

    Session enabled on api middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -41,6 +41,7 @@ class Kernel extends HttpKernel
 
         'api' => [
             \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+            \Illuminate\Session\Middleware\StartSession::class,
             'throttle:api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
